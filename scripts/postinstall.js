fbfa106b1963c29b312c0e64eda32965b8022f65
sato1043 <sato1043@updater.cc>
2025-12-28 17:58:40 +0900

    copy browser-polyfill.js.map in postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -8,6 +8,7 @@ const files = [
   { src: 'node_modules/@truto/turndown-plugin-gfm/lib/index.js', dest: 'turndown-plugin-gfm.mjs' },
   { src: 'node_modules/bulma/css/bulma.css', dest: 'bulma.css' },
   { src: 'node_modules/webextension-polyfill/dist/browser-polyfill.js', dest: 'browser-polyfill.js' },
+  { src: 'node_modules/webextension-polyfill/dist/browser-polyfill.js.map', dest: 'browser-polyfill.js.map' },
   { src: 'node_modules/mustache/mustache.mjs', dest: 'mustache.mjs' },
 ];
 
